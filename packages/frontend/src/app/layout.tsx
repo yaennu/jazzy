ffe6cc7cd129f8e0cf15a4c66b98b9580ec2ab47
Yannick Schwarz <yannick.schwarz@edorex.ch>
2026-01-27 22:24:31 +0100

    feat(frontend): add initial frontend structure with navigation, login, and settings pages

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Navigation from "../components/navigation";
+import Navigation from "@/components/navigation";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
